c9e5fe346238e0e75c99ede7ba0ee0b9fcb465c9
August Wetterau <augustwetterau@Augusts-MacBook-Pro.local>
2024-09-09 16:31:46 -0700

    finished first 3 python parts

--- python/exercises.py
+++ python/exercises.py
@@ -14,13 +14,46 @@ def change(amount: int) -> dict[int, int]:
 
 
 # Write your first then lower case function here
+def first_then_lower_case(a, p):
+    for i in a:
+        if p(i):
+            return i.lower()
+    return None
 
 
 # Write your powers generator here
 
+class powers_generator:
+
+    def __init__(self, base, limit):
+        self.base = base
+        self.limit = limit
+        self.currentNum = 0
+    def __iter__(self):
+        return self
+    def __next__(self):
+        if ((self.base ** self.currentNum) <= self.limit):
+            num = self.base ** self.currentNum
+            self.currentNum += 1
+            return num
+        else:
+            raise StopIteration
+        
+    
+    
+    
 
 # Write your say function here
+def say(word=None):
+    words = []
 
+    def extra(word=None):
+        if word is None:
+            return ' '.join(words)
+        words.append(word)
+        return extra
+
+    return extra(word)
 
 # Write your line count function here
 
